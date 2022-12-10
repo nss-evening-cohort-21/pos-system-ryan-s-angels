a0324b5d6ac59be6cecfac6bddfd8f8cdcf62845
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2022-12-10 11:09:02 -0600

    navBar

--- components/loginButton.js
+++ components/loginButton.js
@@ -1,6 +1,7 @@
 import { signIn } from '../utils/auth';
 
 // GOOGLE LOGIN BUTTON
+
 const loginButton = () => {
   const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
   document.querySelector('#login-form-container').innerHTML = domString;
