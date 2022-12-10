ba1678cce89d0daad7ff68a9a72832e0b6944c36
ryanmbigelow <ryanmbigelow@gmail.com>
2022-12-10 10:50:59 -0600

    created startApp and clearDom

---
+++ utils/startApp.js
@@ -0,0 +1,17 @@
+import logoutButton from '../components/logoutButton';
+import domBuilder from '../components/Shared/domBuilder';
+import navBar from '../components/Shared/navBar';
+import domEvents from '../events/domEvents';
+import formEvents from '../events/formEvents';
+import navigationEvents from '../events/navigationEvents';
+
+const startApp = (user) => {
+  domBuilder(user);
+  domEvents(user);
+  formEvents(user);
+  navBar();
+  logoutButton();
+  navigationEvents(user);
+};
+
+export default startApp;
