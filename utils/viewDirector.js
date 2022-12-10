7f25dca87844e56c7538cfd5ce4c3439b6629c9a
jakehardin <jakehardin13@gmail.com>
2022-12-10 11:31:00 -0600

    added startApp in viewDirector pt.2

--- utils/viewDirector.js
+++ utils/viewDirector.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import loginButton from '../components/loginButton';
-import logoutButton from '../components/logoutButton';
+import startApp from './startApp';
 import client from './client';
 
 const ViewDirectorBasedOnUserAuthStatus = () => {
@@ -9,7 +9,7 @@ const ViewDirectorBasedOnUserAuthStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // person is logged in do something...
-      logoutButton();
+      startApp(user);
     } else {
       // person is NOT logged in
       loginButton();
