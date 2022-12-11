5b539d2215496806c5d415a10ae8df3b1c5a7fea
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2022-12-11 16:42:50 -0600

    homeLoggedIn issue fixed

--- components/buttons/createOrdersButton.js
+++ components/buttons/createOrdersButton.js
@@ -1,6 +1,6 @@
 const createOrdersButton = () => {
   const domString = '<button id="create-order-btn" class="btn btn-warning">NEW ORDER</button>';
-  document.querySelector('#home-logged-in').innerHTML = domString;
+  document.querySelector('#home-logged-in').innerHTML += domString;
   console.warn('YOU CREATED AN ORDER!');
 };
 
