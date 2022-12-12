22f4dc7594a9dda436de430116991c756ea7edfa
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2022-12-12 09:01:22 -0600

    user name shows up on homeLoggedIn

--- components/welcome.js
+++ components/welcome.js
@@ -2,7 +2,7 @@ import renderToDOM from '../utils/renderToDom';
 
 const welcome = (user) => {
   const domString = `
-  <h1>Welcome, ${user}</h1>
+  <h1>Welcome, ${user.displayName}</h1>
   `;
 
   renderToDOM('#home-logged-in', domString);
