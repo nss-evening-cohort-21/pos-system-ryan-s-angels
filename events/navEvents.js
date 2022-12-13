613a2c835e09210a1a4649422e26cffc97506a2d
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2022-12-12 19:18:24 -0600

    view all orders on navbar

--- events/navEvents.js
+++ events/navEvents.js
@@ -1,6 +1,14 @@
+import { showOrders } from '../pages/orders';
 import { signOut } from '../utils/auth';
+import getOrders from '../api/orderData';
 
-const navigationEvents = () => {
+const navigationEvents = (user) => {
+  // VIEW ALL ORDERS
+  document.querySelector('#all-orders')
+    .addEventListener('click', () => {
+      getOrders(user.uid).then(showOrders);
+    });
+
   // LOGOUT BUTTON
   document.querySelector('#logout-button')
     .addEventListener('click', signOut);
