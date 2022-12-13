965a887eff237ff01826e91663b3fe81d2da7708
ryanmbigelow <ryanmbigelow@gmail.com>
2022-12-12 20:39:42 -0600

    created add order form and event listener on DOM button

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,10 +1,14 @@
 import { getOrders } from '../api/orderData';
+import addOrderForm from '../components/forms/addOrderForm';
 import { showOrders } from '../pages/orders';
 
 const domEvents = (user) => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     if (e.target.id.includes('view-order-btn')) {
       getOrders(user.uid).then(showOrders);
+    }
+    if (e.target.id.includes('create-order-btn')) {
+      addOrderForm();
     }
   });
 };
