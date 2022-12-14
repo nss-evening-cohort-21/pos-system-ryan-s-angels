4e3d5a00268195a0909f09a786f1c1a4a3c08120
ryanmbigelow <ryanmbigelow@gmail.com>
2022-12-13 19:26:04 -0600

    got add order form to be fully functional

--- components/forms/addOrderForm.js
+++ components/forms/addOrderForm.js
@@ -1,7 +1,7 @@
 import { clearDom } from '../../utils/clearDom';
 import renderToDOM from '../../utils/renderToDom';
 
-const orderStatusArray = ['Open', 'Closed'];
+const orderTypeArray = ['phone', 'in-person'];
 
 // USING THIS FORM FOR BOTH CREATE AND UPDATE
 const addOrderForm = (obj = {}) => {
@@ -9,20 +9,21 @@ const addOrderForm = (obj = {}) => {
   let domString = `
     <form id="${obj.firebaseKey ? `update-order-btn--${obj.firebaseKey}` : 'submit-order'}" class="mb-4">
       <div class="form-group">
-        <label for="title">Order Name</label>
+        <label for="order-name">Order Name</label>
         <input type="text" class="form-control" id="order-name" aria-describedby="orderName" placeholder="Enter Your Name" value="${obj.order_name || ''}" required>
       </div>
       <div class="form-group">
-        <label for="description">Customer Phone</label>
+        <label for="phone-number">Customer Phone</label>
         <textarea class="form-control" placeholder="Phone Number" id="phone-number" style="height: 100px">${obj.phone_number || ''}</textarea>
       </div>
       <div class="form-group">
-        <label for="description">Customer Email</label>
+        <label for="email">Customer Email</label>
         <textarea class="form-control" placeholder="Email Address" id="email" style="height: 100px">${obj.email || ''}</textarea>
       </div>
+      <label for="order-type">Order Type</label>
       <select class="form-select" id="order-type" aria-label="Default select example" required>
     ;`;
-  orderStatusArray.forEach((order) => {
+  orderTypeArray.forEach((order) => {
     domString += `
       <option
         value="${order}" ${order === obj.order_type ? 'selected' : ''}>${order}
