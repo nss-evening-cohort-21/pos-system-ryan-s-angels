57feb028ca4a1e0fcf76c80d8cdfecc09ffc09d5
jakehardin <jakehardin13@gmail.com>
2022-12-13 18:47:58 -0600

    added event listener to view order button

--- events/domEvents.js
+++ events/domEvents.js
@@ -3,6 +3,8 @@ import addOrderForm from '../components/forms/addOrderForm';
 import { showOrders } from '../pages/orders';
 import getRevenue from '../api/revenueData';
 import { showRevenue } from '../pages/revenue';
+import getOrderDetails from '../api/mergedData';
+import viewOrderDetails from '../pages/viewOrderDetails';
 
 const domEvents = (user) => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -14,6 +16,12 @@ const domEvents = (user) => {
     }
     if (e.target.id.includes('view-revenue-btn')) {
       getRevenue(user.uid).then(showRevenue);
+    }
+    // CLICK EVENT FOR VIEW ORDER DETAILS
+    if (e.target.id.includes('view-order-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      getOrderDetails(firebaseKey).then(viewOrderDetails);
+      console.warn('You clicked the items button!');
     }
   });
 };
