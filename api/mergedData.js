a244ef6adffad797dd0361d0036c2a226e5f3c75
jakehardin <jakehardin13@gmail.com>
2022-12-13 19:06:54 -0600

    page now loads when orderDetails button is clicked

--- api/mergedData.js
+++ api/mergedData.js
@@ -2,9 +2,9 @@ import { getSingleItem } from './itemData';
 import { getOrderItems } from './orderData';
 
 const getOrderDetails = async (firebaseKey) => {
-  const itemObject = await getSingleItem(firebaseKey);
-  const ordersArray = await getOrderItems(firebaseKey);
-  return { ...itemObject, ordersArray };
+  const orderObject = await getSingleItem(firebaseKey);
+  const itemsArray = await getOrderItems(firebaseKey);
+  return { ...orderObject, itemsArray };
 };
 
 export default getOrderDetails;
---
+++ pages/viewOrderDetails.js
@@ -0,0 +1,48 @@
+import { clearDom } from '../utils/clearDom';
+import renderToDOM from '../utils/renderToDom';
+import { emptyOrders } from './orders';
+
+const viewOrderDetails = (orderObject) => {
+  clearDom();
+  if (orderObject.itemsArray.length === 0) {
+    emptyOrders();
+  } else {
+    const str = `
+    <div class="view-author-books-container">
+      <div class="text-white ms-5 details">
+        <h5>${orderObject.first_name} ${orderObject.last_name} ${orderObject.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
+        <div>
+          Author Email: <a href="mailto:${orderObject.email}">${orderObject.email}</a>
+        </div>
+        <hr>
+      </div>
+      <div class="author-books-cards" id="authBooksCards"></div>
+    </div>`;
+    renderToDOM('#store', str);
+    let domString = '';
+    orderObject.itemsArray.forEach((item) => {
+      domString += `
+      <div class="card">
+        <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
+        <div class="card-body" style="height: 250px;">
+          <h6 class="card-title">${item.title}</h6>
+          <p class="card-text bold">${item.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
+          <hr>
+          <div class="book-icons">
+            <i class="btn btn-success"   id="view-book-btn--${item.firebaseKey}">
+              <span><i class= "fas fa-eye"></i></span>
+            </i>
+            <i id="edit-book-btn--${item.firebaseKey}"  class="btn btn-info">
+              <span><i class= "fas fa-edit"></i></span>
+            </i>
+            <i id="delete-book-btn--${item.firebaseKey}"  class="btn btn-danger">
+              <span><i class= "fas fa-trash-alt"></i></span>
+            </i>
+          </div>
+        </div>
+      </div>`;
+    });
+    renderToDOM('#authBooksCards', domString);
+  }
+};
+export default viewOrderDetails;
