0e2416330470ce6d7503adb4ba7f22302a3f148f
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2022-12-14 21:54:06 -0600

    changed name of closeOrder to closeOrderForm, edited formEvents and revenueData

--- events/formEvents.js
+++ events/formEvents.js
@@ -1,5 +1,7 @@
 import { getOrders, createOrder, updateOrder } from '../api/orderData';
 import { showOrders } from '../pages/orders';
+import { updateRevenue, getRevenue } from '../api/revenueData';
+import { showRevenue } from '../pages/revenue';
 
 // GET CURRENT DATE
 const date = new Date();
@@ -49,6 +51,25 @@ const formEvents = (user) => {
 
       updateOrder(payload).then(() => {
         getOrders(user.uid).then(showOrders);
+      });
+    }
+    if (e.target.id.includes('close-order')) {
+      const [, firebaseKey] = e.target.id.split('--');
+
+      const payload = {
+        paymentType: document.querySelector('#payment-type').value,
+        tip: document.querySelector('#tip').value,
+        orderType: document.querySelector('#order-type').value,
+        order_status: 'Closed',
+        // total: order total plus tip,
+        date: currentDate,
+        firebaseKey,
+        uid: user.uid,
+      };
+
+      updateRevenue(payload).then(() => {
+        console.warn(payload);
+        getRevenue(user.uid).then(showRevenue);
       });
     }
   });
